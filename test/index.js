4f72956afd3ebc2c8ab11ff31326dfa6cc79fa11
rafaelcdacunha <corradini@angularlabs.com.br>
2018-08-13 09:49:34 -0300

    dummy test

--- test/index.js
+++ test/index.js
@@ -7,6 +7,6 @@ const path = require('path');
 describe('My package', function () {
   it('should return an array with 4 elements', function () {
     const arr = modelToArray(path.join(__dirname, 'test.model.ts'));
-    expect(arr.lenght).to.equals(4);
+    expect(arr.length).to.equals(4);
   });
 });
